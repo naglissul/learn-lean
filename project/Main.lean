5410a9584e0f276af16e03f35a620346302b834f
naglissul <naglis1134@gmail.com>
2026-08-28 21:04:13 +0200

    done, moving on

--- project/Main.lean
+++
@@ -1,4 +0,0 @@
-import Project
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
--- project/Project.lean
+++
@@ -1,3 +0,0 @@
--- This module serves as the root of the `Project` library.
--- Import modules here that should be built as part of the library.
-import Project.Basic
